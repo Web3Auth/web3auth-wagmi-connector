1f4579f00e7526e536620641fd857dc791253a2e
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-12-08 12:21:57 +0530

    Add whitelabeling in demo

--- examples/demo-with-plugin/src/RainbowWeb3authConnector.js
+++ examples/demo-with-plugin/src/RainbowWeb3authConnector.js
@@ -1,15 +1,18 @@
 import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
 import { TorusWalletConnectorPlugin } from "@web3auth/torus-wallet-connector-plugin";
 
+const name = "My App Name";
+const iconUrl = "https://web3auth.io/docs/contents/logo-ethereum.png";
+
 const torusPlugin = new TorusWalletConnectorPlugin({
   torusWalletOpts: {
     buttonPosition: "bottom-left",
   },
   walletInitOptions: {
     whiteLabel: {
-      theme: { isDark: true, colors: { primary: "#00a8ff" } },
-      logoDark: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-      logoLight: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+      theme: { isDark: false, colors: { primary: "#00a8ff" } },
+      logoDark: iconUrl,
+      logoLight: iconUrl,
     },
     useWalletConnect: true,
     enableLogging: true,
@@ -18,8 +21,8 @@ const torusPlugin = new TorusWalletConnectorPlugin({
 
 export const rainbowWeb3AuthConnector = ({ chains }) => ({
   id: "web3auth",
-  name: "Web3Auth",
-  iconUrl: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+  name,
+  iconUrl,
   iconBackground: "#fff",
   createConnector: () => {
     const connector = new Web3AuthConnector({
@@ -27,8 +30,20 @@ export const rainbowWeb3AuthConnector = ({ chains }) => ({
       options: {
         enableLogging: true,
         clientId: "YOUR_CLIENT_ID", // Get your own client id from https://dashboard.web3auth.io
-        network: "testnet",
-        chainId: "0x1",
+        network: "cyan", // cyan, testnet, mainnet
+        chainId: chains[0].chainId,
+        uiConfig: {
+          theme: "light", // light or dark
+          appLogo: iconUrl,
+        },
+        uxMode: "popup", // popup or redirect
+        whiteLabel: {
+          name,
+          logoLight: iconUrl,
+          logoDark: iconUrl,
+          defaultLanguage: "en",
+          dark: true, // whether to enable dark mode. defaultValue: false
+        },
       },
     });
     connector.web3AuthInstance.addPlugin(torusPlugin);
