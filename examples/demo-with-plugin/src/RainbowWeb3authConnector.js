4cbb050f61ecc4338257b07a11240681a0347dbb
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-12-20 11:58:03 +0530

    Update demo-with-plugin

--- examples/demo-with-plugin/src/RainbowWeb3authConnector.js
+++ examples/demo-with-plugin/src/RainbowWeb3authConnector.js
@@ -1,54 +1,80 @@
 import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
+import { Web3Auth } from "@web3auth/modal";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
 import { TorusWalletConnectorPlugin } from "@web3auth/torus-wallet-connector-plugin";
 
 const name = "My App Name";
 const iconUrl = "https://web3auth.io/docs/contents/logo-ethereum.png";
 
-const torusPlugin = new TorusWalletConnectorPlugin({
-  torusWalletOpts: {
-    buttonPosition: "bottom-left",
-  },
-  walletInitOptions: {
-    whiteLabel: {
-      theme: { isDark: false, colors: { primary: "#00a8ff" } },
-      logoDark: iconUrl,
-      logoLight: iconUrl,
+export const rainbowWeb3AuthConnector = ({ chains }) => {
+  // Create Web3Auth Instance
+  const web3AuthInstance = new Web3Auth({
+    clientId: "YOUR_CLIENT_ID",
+    chainConfig: {
+      chainNamespace: CHAIN_NAMESPACES.EIP155,
+      chainId: chains[0].id.toString(16),
+      rpcTarget: chains[0].rpcUrls.default, // This is the public RPC we have added, please pass on your own endpoint while creating an app
+      displayName: chains[0].name,
+      tickerName: chains[0].nativeCurrency?.name,
+      ticker: chains[0].nativeCurrency?.symbol,
+      blockExplorer: chains[0]?.blockExplorers.default?.url,
     },
-    useWalletConnect: true,
-    enableLogging: true,
-  },
-});
+    uiConfig: {
+      theme: "dark",
+      loginMethodsOrder: ["facebook", "google"],
+      defaultLanguage: "en",
+      appLogo: "https://web3auth.io/images/w3a-L-Favicon-1.svg", // Your App Logo Here
+    },
+  });
 
-export const rainbowWeb3AuthConnector = ({ chains }) => ({
-  id: "web3auth",
-  name,
-  iconUrl,
-  iconBackground: "#fff",
-  createConnector: () => {
-    const connector = new Web3AuthConnector({
-      chains: chains,
-      options: {
-        enableLogging: true,
-        clientId: "YOUR_CLIENT_ID", // Get your own client id from https://dashboard.web3auth.io
-        network: "cyan", // cyan, testnet, mainnet
-        chainId: chains[0].chainId,
-        uiConfig: {
-          theme: "light", // light or dark
-          appLogo: iconUrl,
-        },
-        uxMode: "popup", // popup or redirect
-        whiteLabel: {
-          name,
-          logoLight: iconUrl,
-          logoDark: iconUrl,
-          defaultLanguage: "en",
-          dark: true, // whether to enable dark mode. defaultValue: false
-        },
+  // Add openlogin adapter for customisations
+  const openloginAdapterInstance = new OpenloginAdapter({
+    adapterSettings: {
+      network: "cyan",
+      uxMode: "popup", 
+      whiteLabel: {
+        name: "Your app Name",
+        logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+        logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+        defaultLanguage: "en",
+        dark: true, // whether to enable dark mode. defaultValue: false
       },
-    });
-    connector.web3AuthInstance.addPlugin(torusPlugin);
-    return {
-      connector,
-    };
-  },
-});
+    },
+  });
+  web3AuthInstance.configureAdapter(openloginAdapterInstance);
+
+  // Add Torus Wallet Plugin
+  const torusPlugin = new TorusWalletConnectorPlugin({
+    torusWalletOpts: {
+      buttonPosition: "bottom-left",
+    },
+    walletInitOptions: {
+      whiteLabel: {
+        theme: { isDark: false, colors: { primary: "#00a8ff" } },
+        logoDark: iconUrl,
+        logoLight: iconUrl,
+      },
+      useWalletConnect: true,
+      enableLogging: true,
+    },
+  });
+  web3AuthInstance.addPlugin(torusPlugin);
+  return ({
+    id: "web3auth",
+    name,
+    iconUrl,
+    iconBackground: "#fff",
+    createConnector: () => {
+      const connector = new Web3AuthConnector({
+        chains: chains,
+        options: { 
+          web3AuthInstance,
+        },
+      });
+      return {
+        connector,
+      };
+    },
+  });
+};
